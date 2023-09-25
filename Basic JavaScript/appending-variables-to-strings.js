c13a031e082c3170a72832c446eb0020f1feef3f
JPAG2009 <144880839+JPAG2009@users.noreply.github.com>
2023-09-26 12:35:06 +1300

    Add files via upload

---
+++ Basic JavaScript/appending-variables-to-strings.js
@@ -0,0 +1,4 @@
+// Change code below this line
+const someAdjective = "difficult but rewarding!";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
---
+++ Basic JavaScript/constructing-strings-with-variables.js
@@ -0,0 +1,3 @@
+// Only change code below this line
+const myName = "JPAG";
+const myStr = "My name is" + myName + "i am well";
